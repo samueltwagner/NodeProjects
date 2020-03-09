3e8ce36fd35956cb146e0b5355e5f11a76744c0e
Sam <samuel.thomas.wagner@gmail.com>
2020-03-09 04:01:07 -0600

    week 9

--- web/week9/assignment/app.js
+++ web/week9/assignment/app.js
@@ -11,53 +11,139 @@ app.set('view engine', 'ejs');
 
 // set up a rule that says requests to "/math" should be handled by the
 // handleMath function below
-app.get('/math', handleMath);
+app.get('/postage', calculateRate);
 
 // start the server listening
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
 
+function calculateRate(request, response) {
+	const option = request.query.option;
+	const weight = Number(request.query.weight);
 
-/**********************************************************************
- * Ideally the functions below here would go into a different file
- * but for ease of reading this example and seeing all of the pieces
- * they are listed here.
- **********************************************************************/
-
-function handleMath(request, response) {
-	const operation = request.query.operation;
-	const operand1 = Number(request.query.operand1);
-	const operand2 = Number(request.query.operand2);
-
-	// TODO: Here we should check to make sure we have all the correct parameters
-
-	computeOperation(response, operation, operand1, operand2);
+	computeOperation(response, option, weight);
 }
 
-function computeOperation(response, op, left, right) {
-	op = op.toLowerCase();
+function computeOperation(response, option, weight) {
+	option = option.toLowerCase();
 
-	let result = 0;
+	let price = 0;
 
-	if (op == "add") {
-		result = left + right;
-	} else if (op == "subtract") {
-		result = left - right;		
-	} else if (op == "multiply") {
-		result = left * right;
-	} else if (op == "divide") {
-		result = left / right;
-	} else {
-		// It would be best here to redirect to an "unknown operation"
-		// error page or something similar.
+	if (option == "letters (stamped)") {
+		if (1 <= weight){
+			price = .55;
+		}
+		else if(weight > 1 && weight <= 2){
+			price = .70;
+		}
+		else if(weight > 2 && weight <= 3){
+			price = .85;
+		}
+		else if(weight > 3 && weight <= 3.5){
+			price = 1;
+		}	
+	}
+	else if (option == "letters (metered)") {
+		if (1 <= weight){
+			price = .50;
+		}
+		else if(weight > 1 && weight <= 2){
+			price = .65;
+		}
+		else if(weight > 2 && weight <= 3){
+			price = .80;
+		}
+		else if(weight > 3 && weight <= 3.5){
+			price = .95;
+		}	
+	} else if (option == "large envelopes(flats)") {
+		if (1 <= weight){
+			price = 1;
+		}
+		else if(weight > 1 && weight <= 2){
+			price = 1.2;
+		}
+		else if(weight > 2 && weight <= 3){
+			price = 1.4;
+		}
+		else if(weight > 3 && weight <= 4){
+			price = 1.6;
+		}
+		else if(weight > 4 && weight <= 5){
+			price = 1.8;
+		}
+		else if(weight > 5 && weight <= 6){
+			price = 2;
+		}
+		else if(weight > 6 && weight <= 7){
+			price = 2.2;
+		}
+		else if(weight > 7 && weight <= 8){
+			price = 2.4;
+		}
+		else if(weight > 8 && weight <= 9){
+			price = 2.6;
+		}
+		else if(weight > 9 && weight <= 10){
+			price = 2.8;
+		}
+		else if(weight > 10 && weight <= 11){
+			price = 3;
+		}
+		else if(weight > 11 && weight <= 12){
+			price = 3.2;
+		}
+		else if(weight > 12 && weight <= 13){
+			price = 3.4;
+		}		
+	} else if (option == "first-class package service-retail") {
+		if (1 <= weight){
+			price = 3.8;
+		}
+		else if(weight > 1 && weight <= 2){
+			price = 3.8;
+		}
+		else if(weight > 2 && weight <= 3){
+			price = 3.8;
+		}
+		else if(weight > 3 && weight <= 4){
+			price = 3.8;
+		}
+		else if(weight > 4 && weight <= 5){
+			price = 4.6;
+		}
+		else if(weight > 5 && weight <= 6){
+			price = 4.6;
+		}
+		else if(weight > 6 && weight <= 7){
+			price = 4.6;
+		}
+		else if(weight > 7 && weight <= 8){
+			price = 4.6;
+		}
+		else if(weight > 8 && weight <= 9){
+			price = 5.3;
+		}
+		else if(weight > 9 && weight <= 10){
+			price = 5.3;
+		}
+		else if(weight > 10 && weight <= 11){
+			price = 5.3;
+		}
+		else if(weight > 11 && weight <= 12){
+			price = 5.3;
+		}
+		else if(weight > 12 && weight <= 13){
+			price = 5.9;
+		}		
 	}
 
 	// Set up a JSON object of the values we want to pass along to the EJS result page
-	const params = {operation: op, left: left, right: right, result: result};
+	const params = {option: option, weight: weight, price: price};
 
 	// Render the response, using the EJS page "result.ejs" in the pages directory
 	// Makes sure to pass it the parameters we need.
-	response.render('pages/result', params);
+	response.render('views/result', params);
 
 }
