949867f6de0d5c21bfa6025fefbc828d9fee898d
Sam <samuel.thomas.wagner@gmail.com>
2020-03-09 21:36:42 -0600

    Updated index.js

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ var fs = require('fs');
 // Use the createServer method of your http server object to handle requests.
 function onRequest(req, res) {   
 
+
         if (req.url == "/home")
         {  
             res.writeHead(200, {"Content-Type": "application/json"});
             res.write("<h1>Welcome to the Home Page.</h1>");
         }
-        // **********************************
-        //how to do that???
-        else if(req.week9 == "/week9/assignment/app.js"){
-            app.set('views', __dirname + '/views');
-        }
-        //************************************ */
         else if(req.url == "/getData")
         {
             res.writeHead(200, {"Content-Type": "application/json"});
             res.write("<h1>JSON document that returns name and class</h1>");
+        }
+        //fix tomorrow
+        else if(req.url == "/week9assignment"){
+        fs.readFile('web/week9/assignment/public/form.html', 'utf8',  function(err, data) {
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.write(data);
+        });
         }
         else {
             res.writeHead(404, {"Content-Type": "text/html"});
